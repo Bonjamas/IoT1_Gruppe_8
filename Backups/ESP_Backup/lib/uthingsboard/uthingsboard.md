758c6f29f9a5c11b792461187a3298421f703e17
Bonjamas <benjaminlandling@live.dk>
2024-12-18 15:59:20 +0100

    Opdateret til rapport

--- Backups/ESP_Backup/lib/uthingsboard/uthingsboard.md
+++
@@ -1,3 +0,0 @@
-![[3. Programmering/Python/ittek-main/lib/uthingsboard/client.py]]
-
-![[3. Programmering/Python/ittek-main/lib/uthingsboard/__init__.py]]
--- ID_8/secrets.py
+++
@@ -1,7 +0,0 @@
-# Wifi credentials
-SSID = '1B8'
-PASSWORD = '12345678'
-
-# Server credentials
-ACCESS_TOKEN = 'glxlokqo7jrztzsk97w7'
-SERVER_IP_ADDRESS = '192.168.1.10'
